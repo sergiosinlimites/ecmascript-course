af12d34f4911374cc97eb6a6f6e05550093fc0fa
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2022-12-23 22:39:42 -0500

    finished ES7 features

---
+++ src/es7/array-includes.js
@@ -0,0 +1,6 @@
+const numbers = [1,2,34,4];
+console.log(numbers.includes(2)) // true
+console.log(numbers.includes(2, 2)) // false
+
+const people = ['Sergio', 'Andrés', 'Antony'];
+console.log(people.includes('Sergio'));
---
+++ src/es7/exponential.js
@@ -0,0 +1,2 @@
+const data = 3**12;
+console.log(data)
